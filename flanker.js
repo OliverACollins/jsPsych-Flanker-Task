a0b77344e5dbc6786b270feec60262f890f8cc18
OliverACollins <oliver7collins@outlook.com>
2026-06-16 16:18:42 +0100

    Update instructions

--- flanker.js
+++ flanker.js
@@ -70,8 +70,6 @@ var instructions = {
         <br>
         <p>In this game of speed and reflex, you will need to select the correct response according to the <b>orientation of the middle arrow</b> as fast and as correctly as possible, while <b>resisting the surrounding arrows</b>.</p>
         <br>
-        <p>You have a maximum of <b>2 seconds</b> to respond to each trial.</p>
-        <br>
         <p>You will first have a chance to practice this task. Press "Continue" to start the practice trials. The block will begin with a <b>3 - 2 - 1</b> countdown.</p>
     `
 }
@@ -278,8 +276,6 @@ var begin = {
         <p>When the <b>MIDDLE arrow</b> points to the <b>right (>)</b>, press the <b>RIGHT arrow key</b> on the keyboard</p>
         <br>
         <p>In this game of speed and reflex, you will need to select the correct response according to the <b>orientation of the middle arrow</b> as fast and as correctly as possible, while <b>resisting the surrounding arrows</b>.</p>
-        <br>
-        <p>You have a maximum of 2 seconds to respond to each trial.</p>
         <br>
         <p>Press "Continue" to start the experimental trials. Each block will begin with a <b>3 - 2 - 1</b> countdown.</p>
     `
